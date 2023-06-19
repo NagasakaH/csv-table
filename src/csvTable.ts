45254b80e727be1e7aec3f5fb951a2fa4c6a05a2
Hiroaki Nagasaka <hiroaki.nagasaka37@gmail.com>
2023-06-19 09:10:23 +0900

    二つ以上のテーブルを正常に描画できない不具合修正

--- src/csvTable.ts
+++ src/csvTable.ts
@@ -12,15 +12,15 @@ const loadCSV = async (filePath: string, id: string, options?: Options) => {
   const data = await csv.fromString(text);
   const targetId = `#${id}`;
   if (!options) {
-    const tabulator = new Tabulator(`#${id}`, {
+    const tabulator = new Tabulator(targetId, {
       data: data,
       autoColumns: true,
       layout: 'fitColumns',
     });
   } else {
-    const tabulator = new Tabulator(`#${id}`, {
-      ...options,
+    const tabulator = new Tabulator(targetId, {
       layout: 'fitColumns',
+      ...options,
       data: data,
     });
   }
@@ -28,6 +28,11 @@ const loadCSV = async (filePath: string, id: string, options?: Options) => {
 
 export function csvTable(options: {filename: string; options?: Options}[]) {
   const images = document.getElementsByTagName('img');
+  const replacePairs: {
+    parent: HTMLElement;
+    to: HTMLDivElement;
+    from: HTMLImageElement;
+  }[] = [];
   for (let i = 0; i < images.length; i++) {
     const image = images[i];
     const match = /.*\.csv/;
@@ -35,15 +40,21 @@ export function csvTable(options: {filename: string; options?: Options}[]) {
       const csvTable = document.createElement('div');
       csvTable.id = 'csv-table' + i.toString();
       const parent = image.parentElement;
-      parent?.replaceChild(csvTable, image);
-      const optionCandidates = options?.filter(option => {
-        return option.filename === image.src;
-      });
-      const selectedOptions =
-        optionCandidates && optionCandidates.length > 0
-          ? optionCandidates[0]
-          : undefined;
-      loadCSV(image.src, csvTable.id, selectedOptions?.options);
+      if (parent) {
+        replacePairs.push({parent: parent, to: csvTable, from: image});
+      }
     }
+  }
+  for (const pair of replacePairs) {
+    const src = pair.from.src;
+    pair.parent.replaceChild(pair.to, pair.from);
+    const optionCandidates = options?.filter(option => {
+      return option.filename === src;
+    });
+    const selectedOptions =
+      optionCandidates && optionCandidates.length > 0
+        ? optionCandidates[0]
+        : undefined;
+    loadCSV(src, pair.to.id, selectedOptions?.options);
   }
 }
